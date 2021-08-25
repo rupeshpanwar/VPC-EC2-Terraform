0c15e0cbdf4447e04ffd25501980135a4077faf2
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-25 16:01:19 +0530

    private ELB

---
+++ platform/11-private-elb.tf
@@ -0,0 +1,26 @@
+
+resource "aws_elb" "backend-load-balancer" {
+  name            = "Production-Backend-LoadBalancer"
+  internal        = true
+  security_groups = [aws_security_group.elb_security_group.id]
+  subnets         = [
+    data.terraform_remote_state.network_configuration.outputs.private-subnet-1_id,
+    data.terraform_remote_state.network_configuration.outputs.private-subnet-2_id,
+    data.terraform_remote_state.network_configuration.outputs.private-subnet-3_id
+  ]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
+  }
+
+  health_check {
+    healthy_threshold = 5
+    interval = 30
+    target = "HTTP:80/index.html"
+    timeout = 10
+    unhealthy_threshold = 5
+  }
+}
