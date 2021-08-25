30c86bcd5d00d50767bdc6890850e64e45f90003
Rupesh Panwar <75510135+rupeshpanwar@users.noreply.github.com>
2021-08-25 17:18:34 +0530

    Update 07-ami-info.tf

--- platform/07-ami-info.tf
+++ platform/07-ami-info.tf
@@ -1,8 +1,24 @@
-data "aws_ami" "launch_configuration_ami" {
-    most_recent = true
+# data "aws_ami" "launch_configuration_ami" {
+#     most_recent = true
+#     owners      = ["self"]
 
-    filter {
-        name = "owner-alias"
-        values = ["amazon"]
-    }
+#     filter {
+#         name = "owner-alias"
+#         values = ["amazon"]
+#     }
+# }
+
+data "aws_ami" "launch_configuration_ami" {
+most_recent = true
+owners = ["099720109477"] # Canonical
+
+  filter {
+      name   = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+  }
+
+  filter {
+      name   = "virtualization-type"
+      values = ["hvm"]
+  }
 }
