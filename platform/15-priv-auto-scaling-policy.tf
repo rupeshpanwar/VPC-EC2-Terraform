4de0c8b6f246072bb0e59de85dcd70b1cfc39df4
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-25 16:27:25 +0530

    SNS

---
+++ platform/15-priv-auto-scaling-policy.tf
@@ -0,0 +1,14 @@
+
+resource "aws_autoscaling_policy" "backend-production-scale-up-policy" {
+  autoscaling_group_name   = aws_autoscaling_group.ec2_private_autoscaling_group.name
+  name                     = "Production-Backend-AutoScaling-Policy"
+  policy_type              = "TargetTrackingScaling"
+  min_adjustment_magnitude = 1
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 80.0
+  }
+}
