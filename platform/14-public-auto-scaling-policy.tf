a6378f597eff0e4df7427d1f4bdd37167e8fca20
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-25 16:16:03 +0530

    Public auto scaling policy

---
+++ platform/14-public-auto-scaling-policy.tf
@@ -0,0 +1,14 @@
+
+resource "aws_autoscaling_policy" "webapp-production-scale-up-policy" {
+  autoscaling_group_name   = aws_autoscaling_group.ec2_public_autoscaling_group.name
+  name                     = "Production-WebApp-AutoScaling-Policy"
+  policy_type              = "TargetTrackingScaling"
+  min_adjustment_magnitude = 1
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 80.0
+  }
+}
