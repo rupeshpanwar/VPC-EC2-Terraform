5ecac3d0a680b632bb1705fb4e58edfdea43f26f
Rupesh Panwar <rupeshpanwar28@gmail.com>
2021-08-25 11:32:19 +0530

    remote backend and  vpc

---
+++ infrastructure/01-vpc.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  backend "s3" {}
+}
+
+resource "aws_vpc" "production-vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Production-VPC"
+  }
+}
